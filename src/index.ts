bf4771a1427c89433beb4dff460962aa30f00252
wangshaolin <wangshaolin@kuaishou.com>
2022-09-22 11:20:48 +0800

    feat: add useMemoizedFn hook

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as usePortal } from './hooks/usePortal';
 export { default as useForceUpdate } from './hooks/useForceUpdate';
 export { default as usePromise } from './hooks/usePromise';
+export { default as useMemoizedFn } from './hooks/useMemoizedFn';
+
 
